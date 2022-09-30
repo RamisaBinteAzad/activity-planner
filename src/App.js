93c0e91c7e5f02ae6953b8f3e68bbf784eb9efd0
RamisaAzad <tasnimlamia52@gmail.com>
2022-09-30 20:18:05 +0600

    Toast is Added

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   return (
     <div >
       <ActivitiesPlan></ActivitiesPlan>
-       
+       FrequentlyAskedQuestion
       
     </div>
 
---
+++ src/components/FrequentlyAskedQuestion/FrequentlyAskedQuestion.js
@@ -0,0 +1,11 @@
+ import React from 'react';
+ 
+ const FrequentlyAskedQuestion = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+ };
+ 
+ export default FrequentlyAskedQuestion;
