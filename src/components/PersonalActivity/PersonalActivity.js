1b63919f8d532d342130b62d25a37b3fa2d42d9b
RamisaAzad <tasnimlamia52@gmail.com>
2022-09-30 16:36:11 +0600

    Add Break time By Clicking Button

--- src/components/PersonalActivity/PersonalActivity.js
+++ src/components/PersonalActivity/PersonalActivity.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './PersonalActivity.css'
 
 const PersonalActivity = () => {
+    const [breakTime, setBreakTime] = useState(0);
+    const addBreakTime = (event) => {
+        
+        event.target.style.backgroundColor = "green";
+        
+        event = event.target.innerText;
+         
+        let number = event.match(/\d/g);
+        
+        number = number.join("")
+         
+         
+        setBreakTime(number);
+         
+    } 
+   
+     
+    
+    
     return (
         <div className='personal-activity'>
             <div className='container  ps-5 d-flex  align-items-center  pt-4'>
@@ -13,6 +32,7 @@ const PersonalActivity = () => {
 
 
             </div>
+      
             <div className=" py-3 mb-5 mt-5 mx-3 border border-light bg-personal-activity-container rounded mt-4 text-center">
                 <div class="row  ">
                     <div class="col">
@@ -46,20 +66,20 @@ const PersonalActivity = () => {
             <div className=" py-3 pe-1   mb-4 mx-3 border border-light bg-light rounded mt-4 text-center">
                 <div className="row row-cols-5 ">
                     <div className="col">
-                        <button className="btn btn btn-primary py-2 px-1 rounded-circle" type="submit">10m</button>
+                        <button  className="btn btn btn-primary py-2 px-1 rounded-circle" type="submit" onClick={event => addBreakTime(event)}>10m</button>
                     </div>
                     <div className="col">
-                        <button className="btn btn btn-primary py-2 px-1 rounded-circle" type="submit">20m</button>
+                        <button className="btn btn btn-primary py-2 px-1 rounded-circle" type="submit" onClick={event => addBreakTime(event)}>20m</button>
 
                     </div>
                     <div className="col">
-                        <button className="btn btn btn-primary py-2 px-1 rounded-circle" type="submit">30m</button>
+                        <button className="btn btn btn-primary py-2 px-1 rounded-circle" type="submit" onClick={event => addBreakTime(event)}>30m</button>
                     </div>
                     <div className="col">
-                        <button className="btn btn btn-primary py-2 px-1 rounded-circle" type="submit">40m</button>
+                        <button className="btn btn btn-primary py-2 px-1 rounded-circle" type="submit" onClick={event => addBreakTime(event)}>40m</button>
                     </div>
                     <div className="col">
-                        <button className="btn btn btn-primary py-2 px-1 rounded-circle" type="submit">50m</button>
+                        <button className="btn btn btn-primary py-2 px-1 rounded-circle" type="submit" onClick={event => addBreakTime(event)}>50m</button>
                     </div>
                 </div>
                  
@@ -87,7 +107,7 @@ const PersonalActivity = () => {
                          
                     </div>
                     <div class="col">
-                        <span className='text-muted  h6'>0 minutes</span>
+                        <span className='text-muted  h6'>{breakTime} minutes </span>
                          
                     </div>
                     
